ad7a60bbb11cd4e5a625750b3a1a887958042275
iayanalistar <iayanali2004@gmail.com>
2026-04-17 01:50:39 -0500

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -6,12 +6,11 @@ function validateFirstName() {
         return false;
     }
     else if (!/^[A-Za-z'-]{1,30}$/.test(x)) {
-        
-            document.getElementById("fnameError").innerHTML = "Letters, apostrophes, and dashes only";
+        document.getElementById("fnameError").innerHTML = "Letters, apostrophes, and dashes only";
         return false;
     }
     else {
-            document.getElementById("fnameError").innerHTML = "";    
+        document.getElementById("fnameError").innerHTML = "";
         return true;
     }
 }
@@ -19,171 +18,106 @@ function validateFirstName() {
 function validateMI() {
     var x = document.getElementById("mi").value;
 
-        if  (x == "") {
+    if (x == "") {
         document.getElementById("miError").innerHTML = "";
         return true;
     }
     else if (!/^[A-Za-z]$/.test(x)) {
         document.getElementById("miError").innerHTML = "Middle initial must be one letter";
-        
         return false;
-        }
+    }
     else {
         document.getElementById("miError").innerHTML = "";
         return true;
     }
-    }
+}
 
 function validateLastName() {
     var x = document.getElementById("lname").value;
 
     if (x == "") {
-            document.getElementById("lnameError").innerHTML = "Last name is required";
+        document.getElementById("lnameError").innerHTML = "Last name is required";
         return false;
     }
     else if (!/^[A-Za-z'-]{1,30}$/.test(x)) {
-        
         document.getElementById("lnameError").innerHTML = "Letters, apostrophes, and dashes only";
-            return false;
+        return false;
     }
     else {
         document.getElementById("lnameError").innerHTML = "";
-            return  true;
+        return true;
     }
-    }
+}
 
 function validateDOB() {
     var x = document.getElementById("dob").value;
 
     if (x == "") {
         document.getElementById("dobError").innerHTML = "Date of birth is required";
-            return false;
+        return false;
     }
 
-    var dob =  new Date(x);
-    
+    var dob = new Date(x);
     var today = new Date();
-    
     var oldDate = new Date();
     oldDate.setFullYear(today.getFullYear() - 120);
 
     if (dob > today) {
-         document.getElementById("dobError").innerHTML = "DOB cannot be in the future";
+        document.getElementById("dobError").innerHTML = "DOB cannot be in the future";
         return false;
     }
-        
-    else  if (dob < oldDate) {
+    else if (dob < oldDate) {
         document.getElementById("dobError").innerHTML = "DOB cannot be more than 120 years ago";
         return false;
-          }
+    }
     else {
         document.getElementById("dobError").innerHTML = "";
         return true;
-        
-        }
+    }
 }
+
 function validateSSN() {
-        var x = document.getElementById("ssn").value;
+    var x = document.getElementById("ssn").value;
 
     if (x == "") {
-            document.getElementById("ssnError").innerHTML = "Social Security is required";
+        document.getElementById("ssnError").innerHTML = "Social Security is required";
         return false;
-        }
+    }
     else if (!/^[0-9]{9}$/.test(x)) {
         document.getElementById("ssnError").innerHTML = "Enter exactly 9 digits";
-        return     false;
+        return false;
     }
     else {
         document.getElementById("ssnError").innerHTML = "";
-        
-            return true;
-    }
-}
-
-function validateZip() {
-    var x = document.getElementById("zip").value;
-
-    
-
-    if (x == "") {
-        document.getElementById("zipError").innerHTML = "ZIP code is required";
-            return false;
-    }
-    else if   (!/^[0-9]{5}$/.test(x)) {
-        document.getElementById("zipError").innerHTML = "ZIP code must be 5 digits";
-            return false;
-    }
-    else {
-        document.getElementById("zipError").innerHTML = "";
-            return true;
-    }
-
-    
-    }
-
-    function validatePhone() {
-    var x = document.getElementById("phone").value;
-
-    if (x == "") {
-        document.getElementById("phoneError").innerHTML = "Phone number is required";
-        return false;
-        }
-    else if (!/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(x)) {
-           document.getElementById("phoneError").innerHTML = "Use format 123-456-7890";
-        return false;
-    }
-    else     {
-        document.getElementById("phoneError").innerHTML = "";
         return true;
     }
 }
 
-function validateEmail() {
-    var field = document.getElementById("email");
-        field.value = field.value.toLowerCase();
-    var x = field.value;
-
-    if (x == "") {
-        document.getElementById("emailError").innerHTML = "Email is required";
-        return false;
-    }
-    else if (!/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(x)) {
-        document.getElementById("emailError").innerHTML = "Enter a valid email address";
-        return false;
-    }
-    else     {
-        document.getElementById("emailError").innerHTML = "";
-        return true;
-    }
-}
 function validateAddress1() {
-        var x = document.getElementById("address1").value;
+    var x = document.getElementById("address1").value;
 
     if (x == "") {
-            document.getElementById("address1Error").innerHTML = "Street address is required";
-            return false;
+        document.getElementById("address1Error").innerHTML = "Street address is required";
+        return false;
     }
     else if (x.length < 2 || x.length > 30) {
         document.getElementById("address1Error").innerHTML = "Street address must be 2 to 30 characters";
-            return false;
+        return false;
     }
     else {
         document.getElementById("address1Error").innerHTML = "";
-            return true;
+        return true;
     }
 }
-
-
-
 
 function validateCity() {
     var x = document.getElementById("city").value;
 
     if (x == "") {
-           document.getElementById("cityError").innerHTML = "City is required";
+        document.getElementById("cityError").innerHTML = "City is required";
         return false;
-}
-     else if (x.length < 2 || x.length > 30) {
+    }
+    else if (x.length < 2 || x.length > 30) {
         document.getElementById("cityError").innerHTML = "City must be 2 to 30 characters";
         return false;
     }
@@ -191,7 +125,7 @@ function validateCity() {
         document.getElementById("cityError").innerHTML = "";
         return true;
     }
- }
+}
 
 function validateState() {
     var x = document.getElementById("state").value;
@@ -206,46 +140,99 @@ function validateState() {
     }
 }
 
-function validateUserId() {
-            var field = document.getElementById("userid");
-            field.value = field.value.toLowerCase();
-            var x = field.value;
+function validateZip() {
+    var x = document.getElementById("zip").value;
 
-            if (x == "") {
-                document.getElementById("useridError").innerHTML = "User ID is required";
-                return false;
-        }
+    if (x == "") {
+        document.getElementById("zipError").innerHTML = "ZIP code is required";
+        return false;
+    }
+    else if (!/^[0-9]{5}$/.test(x)) {
+        document.getElementById("zipError").innerHTML = "ZIP code must be 5 digits";
+        return false;
+    }
+    else {
+        document.getElementById("zipError").innerHTML = "";
+        return true;
+    }
+}
+
+function validatePhone() {
+    var x = document.getElementById("phone").value;
+
+    if (x == "") {
+        document.getElementById("phoneError").innerHTML = "Phone number is required";
+        return false;
+    }
+    else if (!/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(x)) {
+        document.getElementById("phoneError").innerHTML = "Use format 123-456-7890";
+        return false;
+    }
+    else {
+        document.getElementById("phoneError").innerHTML = "";
+        return true;
+    }
+}
+
+function validateEmail() {
+    var field = document.getElementById("email");
+    field.value = field.value.toLowerCase();
+    var x = field.value;
+
+    if (x == "") {
+        document.getElementById("emailError").innerHTML = "Email is required";
+        return false;
+    }
+    else if (!/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(x)) {
+        document.getElementById("emailError").innerHTML = "Enter a valid email address";
+        return false;
+    }
+    else {
+        document.getElementById("emailError").innerHTML = "";
+        return true;
+    }
+}
+
+function validateUserId() {
+    var field = document.getElementById("userid");
+    field.value = field.value.toLowerCase();
+    var x = field.value;
+
+    if (x == "") {
+        document.getElementById("useridError").innerHTML = "User ID is required";
+        return false;
+    }
     else if (!/^[a-z][a-z0-9_-]{4,19}$/.test(x)) {
         document.getElementById("useridError").innerHTML = "5-20 characters, start with a letter, no spaces";
         return false;
     }
-      else {
+    else {
         document.getElementById("useridError").innerHTML = "";
         return true;
     }
 }
 
 function validatePassword() {
-        var x = document.getElementById("password").value;
-        var userid = document.getElementById("userid").value.toLowerCase();
+    var x = document.getElementById("password").value;
+    var userid = document.getElementById("userid").value.toLowerCase();
 
     if (x == "") {
         document.getElementById("passwordError").innerHTML = "Password is required";
         return false;
     }
     else if (x.length < 8) {
-            document.getElementById("passwordError").innerHTML = "Password must be at least 8 characters";
-            return false;
+        document.getElementById("passwordError").innerHTML = "Password must be at least 8 characters";
+        return false;
     }
     else if (!/[A-Z]/.test(x)) {
-            document.getElementById("passwordError").innerHTML = "Password needs an uppercase letter";
-            return false;
+        document.getElementById("passwordError").innerHTML = "Password needs an uppercase letter";
+        return false;
     }
     else if (!/[a-z]/.test(x)) {
         document.getElementById("passwordError").innerHTML = "Password needs a lowercase letter";
         return false;
     }
-    else if (!/[0-9]/.test(x))  {
+    else if (!/[0-9]/.test(x)) {
         document.getElementById("passwordError").innerHTML = "Password needs a number";
         return false;
     }
@@ -253,15 +240,13 @@ function validatePassword() {
         document.getElementById("passwordError").innerHTML = "Password cannot equal the user ID";
         return false;
     }
-    else  {
+    else {
         document.getElementById("passwordError").innerHTML = "";
-        
         return true;
     }
-    }
+}
 
-    function validateConfirmPassword() {
-        
+function validateConfirmPassword() {
     var x = document.getElementById("confirmPassword").value;
     var y = document.getElementById("password").value;
 
@@ -272,18 +257,13 @@ function validatePassword() {
     else if (x != y) {
         document.getElementById("confirmPasswordError").innerHTML = "Passwords do not match";
         return false;
- }
+    }
     else {
         document.getElementById("confirmPasswordError").innerHTML = "";
         return true;
     }
 }
 
-
-
-
-function validateForm() {
-    
 function validateForm() {
     var ok = true;
     var badFields = "";
@@ -360,44 +340,34 @@ function validateForm() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-
-     document.getElementById("fname").addEventListener("input", validateFirstName);
+    document.getElementById("fname").addEventListener("input", validateFirstName);
     document.getElementById("mi").addEventListener("input", validateMI);
     document.getElementById("lname").addEventListener("input", validateLastName);
     document.getElementById("dob").addEventListener("change", validateDOB);
     document.getElementById("ssn").addEventListener("input", validateSSN);
-    document.getElementById("zip").addEventListener("input", validateZip);
-    document.getElementById("phone").addEventListener("input", validatePhone);
-    document.getElementById("email").addEventListener("input", validateEmail);
     document.getElementById("address1").addEventListener("input", validateAddress1);
     document.getElementById("city").addEventListener("input", validateCity);
     document.getElementById("state").addEventListener("change", validateState);
+    document.getElementById("zip").addEventListener("input", validateZip);
+    document.getElementById("phone").addEventListener("input", validatePhone);
+    document.getElementById("email").addEventListener("input", validateEmail);
     document.getElementById("userid").addEventListener("input", validateUserId);
     document.getElementById("password").addEventListener("input", validatePassword);
     document.getElementById("confirmPassword").addEventListener("input", validateConfirmPassword);
-    
-    const reviewBtn = document.getElementById("reviewBtn");
-    const validateBtn = document.getElementById("validateBtn");
-    const submitBtn = document.getElementById("submitBtn");
-    const userIdField = document.getElementById("userid");
-    const passwordField = document.getElementById("password");
-    const confirmPasswordField = document.getElementById("confirmPassword");
-    const reviewContent = document.getElementById("reviewContent");
 
-     if (validateBtn) {
+    var reviewBtn = document.getElementById("reviewBtn");
+    var validateBtn = document.getElementById("validateBtn");
+    var resetBtn = document.getElementById("resetBtn");
+    var reviewContent = document.getElementById("reviewContent");
+
+    if (validateBtn) {
         validateBtn.addEventListener("click", function () {
             validateForm();
         });
-     }
-
-    if (userIdField) {
-        userIdField.addEventListener("input", function () {
-            this.value = this.value.toLowerCase();
-        });
-        
     }
 
-        document.getElementById("resetBtn").addEventListener("click", function () {
+    if (resetBtn) {
+        resetBtn.addEventListener("click", function () {
             document.getElementById("submitBtn").style.display = "none";
             document.getElementById("formMessage").innerHTML = "";
             document.getElementById("fnameError").innerHTML = "";
@@ -414,69 +384,49 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("useridError").innerHTML = "";
             document.getElementById("passwordError").innerHTML = "";
             document.getElementById("confirmPasswordError").innerHTML = "";
-});
-
-    function checkPasswordsMatch() {
-        if (!passwordField || !confirmPasswordField) return;
-
-        if (confirmPasswordField.value === "") {
-            confirmPasswordField.setCustomValidity("");
-        } else if (passwordField.value !== confirmPasswordField.value) {
-            confirmPasswordField.setCustomValidity("Passwords do not match.");
-        } else {
-            confirmPasswordField.setCustomValidity("");
-        }
-    }
-
-    if (passwordField && confirmPasswordField) {
-        passwordField.addEventListener("input", checkPasswordsMatch);
-        confirmPasswordField.addEventListener("input", checkPasswordsMatch);
+            if (reviewContent) {
+                reviewContent.innerHTML = "";
+            }
+        });
     }
 
     if (reviewBtn) {
         reviewBtn.addEventListener("click", function () {
-            checkPasswordsMatch();
+            var fname = document.getElementById("fname").value;
+            var mi = document.getElementById("mi").value;
+            var lname = document.getElementById("lname").value;
+            var dob = document.getElementById("dob").value;
+            var ssn = document.getElementById("ssn").value;
+            var email = document.getElementById("email").value;
+            var phone = document.getElementById("phone").value;
+            var address1 = document.getElementById("address1").value;
+            var city = document.getElementById("city").value;
+            var state = document.getElementById("state").value;
+            var zip = document.getElementById("zip").value;
+            var userid = document.getElementById("userid").value;
+            var salary = document.getElementById("salary").value;
+            var comments = document.getElementById("comments").value;
 
-            let fname = document.getElementById("fname")?.value || "";
-            let mi = document.getElementById("mi")?.value || "";
-            let lname = document.getElementById("lname")?.value || "";
-            let dob = document.getElementById("dob")?.value || "";
-            let ssn = document.getElementById("ssn")?.value || "";
-            let email = document.getElementById("email")?.value || "";
-            let phone = document.getElementById("phone")?.value || "";
-            let address1 = document.getElementById("address1")?.value || "";
-            let city = document.getElementById("city")?.value || "";
-            let state = document.getElementById("state")?.value || "";
-            let zip = document.getElementById("zip")?.value || "";
-            let userid = document.getElementById("userid")?.value || "";
-            let salary = document.getElementById("salary")?.value || "";
-            let comments = document.getElementById("comments")?.value || "";
+            var vaccinated = "Not selected";
+            var vaccinatedChecked = document.querySelector('input[name="vaccinated"]:checked');
+            if (vaccinatedChecked) {
+                vaccinated = vaccinatedChecked.value;
+            }
 
-            let vaccinated = document.querySelector('input[name="vaccinated"]:checked')?.value || "Not selected";
-
-            let conditions = [];
+            var history = [];
             document.querySelectorAll('input[name="history"]:checked').forEach(function (box) {
-                conditions.push(box.value);
+                history.push(box.value);
             });
 
-            if (zip.length > 5) {
-                zip = zip.substring(0, 5);
-            }
-            
-   
-            
-
-            let output = `
+            var output = `
                 <h3>PLEASE REVIEW THIS INFORMATION</h3>
                 <p><strong>Name:</strong> ${fname} ${mi} ${lname}</p>
                 <p><strong>Date of Birth:</strong> ${dob}</p>
                 <p><strong>ID Number:</strong> ${ssn}</p>
                 <p><strong>Email:</strong> ${email}</p>
                 <p><strong>Phone:</strong> ${phone}</p>
-                <p><strong>Address:</strong><br>
-                ${address1}<br>
-                ${city}, ${state} ${zip}</p>
-                <p><strong>Conditions:</strong> ${conditions.length ? conditions.join(", ") : "None selected"}</p>
+                <p><strong>Address:</strong><br>${address1}<br>${city}, ${state} ${zip}</p>
+                <p><strong>Medical History:</strong> ${history.length ? history.join(", ") : "None selected"}</p>
                 <p><strong>Vaccinated:</strong> ${vaccinated}</p>
                 <p><strong>Desired Salary:</strong> $${salary}</p>
                 <p><strong>Comments / Symptoms:</strong> ${comments}</p>
